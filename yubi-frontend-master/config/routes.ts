ec0c49ee1c1ec7c80f3dd335e438124cd7d0b589
hwwzrzr <486476918@qq.com>
2023-07-13 20:21:51 +0800

    init project

---
+++ yubi-frontend-master/config/routes.ts
@@ -0,0 +1,18 @@
+﻿export default [
+  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { path: '/', redirect: '/add_chart' },
+  { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
+  { path: '/add_chart_async', name: '智能分析（异步）', icon: 'barChart', component: './AddChartAsync' },
+  { path: '/my_chart', name: '我的图表', icon: 'pieChart', component: './MyChart' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', name: '管理页面', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '管理页面2', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
