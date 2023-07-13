b6292afdf00299c3787d9b3ae0c2bcdc38e529ee
hwwzrzr <486476918@qq.com>
2023-07-13 22:34:22 +0800

    init v2

--- Bi-backend/src/main/java/com/yupi/springbootinit/aop/AuthInterceptor.java
+++ Bi-backend/src/main/java/com/yupi/springbootinit/aop/AuthInterceptor.java
@@ -47,22 +47,22 @@ public class AuthInterceptor {
         // 必须有该权限才通过
         if (StringUtils.isNotBlank(mustRole)) {
             UserRoleEnum mustUserRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
-        if (mustUserRoleEnum == null) {
-            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-        }
-        String userRole = loginUser.getUserRole();
-        // 如果被封号，直接拒绝
-        if (UserRoleEnum.BAN.equals(mustUserRoleEnum)) {
-            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-        }
-        // 必须有管理员权限
-        if (UserRoleEnum.ADMIN.equals(mustUserRoleEnum)) {
-            if (!mustRole.equals(userRole)) {
+            if (mustUserRoleEnum == null) {
                 throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
             }
+            String userRole = loginUser.getUserRole();
+            // 如果被封号，直接拒绝
+            if (UserRoleEnum.BAN.equals(mustUserRoleEnum)) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+            // 必须有管理员权限
+            if (UserRoleEnum.ADMIN.equals(mustUserRoleEnum)) {
+                if (!mustRole.equals(userRole)) {
+                    throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+                }
+            }
         }
-    }
-    // 通过权限校验，放行
+        // 通过权限校验，放行
         return joinPoint.proceed();
     }
 }
