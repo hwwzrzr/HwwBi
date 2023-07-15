5e92b5ed96fbffbe143933bf3965a2ef287e13ab
hwwzrzr <486476918@qq.com>
2023-07-15 10:19:28 +0800

    Excel to CSV and compress data

---
+++ Bi-backend/src/main/java/com/yupi/springbootinit/utils/ExcelUtils.java
@@ -0,0 +1,9 @@
+package com.yupi.springbootinit.utils;/**
+*@ClassName ExcelUtils
+*@Description  TODO
+*@Author Administrator
+*@Date 2023/7/15 9:33
+*@Version 1.0
+*/
+public class ExcelUtils {
+}
